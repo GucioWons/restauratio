b98eea28408858c2e121026a130ef3773ef14f7e
GucioWons <wgutek1@wp.pl>
2025-02-07 21:04:00 +0100

    Table-entity

--- restauratio-server/src/main/java/com/guciowons/restauratio/table/Table.java
+++ restauratio-server/src/main/java/com/guciowons/restauratio/table/Table.java
@@ -1,4 +1,13 @@
 package com.guciowons.restauratio.table;
 
+import com.guciowons.restauratio.restaurant.Restaurant;
+
+import java.util.UUID;
+
 public class Table {
+    private UUID id;
+
+    private String name;
+
+    private Restaurant restaurant;
 }
