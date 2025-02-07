b0be2bcd29f528f8e5ce850e78f3b78b9e122d43
GucioWons <wgutek1@wp.pl>
2025-02-07 20:44:43 +0100

    Initial-commit

---
+++ restauratio-server/src/main/java/com/guciowons/restauratio/RestauratioApplication.java
@@ -0,0 +1,13 @@
+package com.guciowons.restauratio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauratioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauratioApplication.class, args);
+	}
+
+}
