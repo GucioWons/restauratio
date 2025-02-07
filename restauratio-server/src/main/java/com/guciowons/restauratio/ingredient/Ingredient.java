5a37672894f2c0c81373ed5ede14389073e61685
GucioWons <wgutek1@wp.pl>
2025-02-07 21:10:43 +0100

    Ingredient-entity

--- restauratio-server/src/main/java/com/guciowons/restauratio/ingredient/Ingredient.java
+++ restauratio-server/src/main/java/com/guciowons/restauratio/ingredient/Ingredient.java
@@ -1,4 +1,16 @@
 package com.guciowons.restauratio.ingredient;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Ingredient {
+    private UUID id;
+
+    private String name;
 }
