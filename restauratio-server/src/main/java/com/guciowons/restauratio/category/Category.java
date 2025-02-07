051eb863990f2aff5dbc6307f961bbd264549a1b
GucioWons <wgutek1@wp.pl>
2025-02-07 21:07:56 +0100

    Category-entity

--- restauratio-server/src/main/java/com/guciowons/restauratio/category/Category.java
+++ restauratio-server/src/main/java/com/guciowons/restauratio/category/Category.java
@@ -1,4 +1,19 @@
 package com.guciowons.restauratio.category;
 
+import com.guciowons.restauratio.restaurant.Restaurant;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Category {
+    private UUID id;
+
+    private String name;
+
+    private Restaurant restaurant;
 }
